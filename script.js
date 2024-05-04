b4ff59604c521696617c68c73704827b77f3a918
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-05-04 06:59:10 +0500

    REVEALING-ELEMENTS-ON-SCROLL

--- script.js
+++ script.js
@@ -246,6 +246,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+// REVEAL SECTIONS
+// REVEALING ELEMENTS ON SCROLL
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hiddden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
